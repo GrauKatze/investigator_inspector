8c7625da86dc796c7277b3cf1ac2753e08a882e4
GrauKatze <GrauKatze@protonmail.com>
2026-03-05 10:36:01 +0300

    refactor code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,59 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+enum Config {
+    Version,
+    Help,
+    Command(Option<Vec<String>>),
+}
+
+impl Config {
+    fn build(init_options: &Option<Vec<String>>) -> Result<Config, String> {
+        match init_options {
+            None => Ok(Config::Help),
+            Some(options) => match options.get(0) {
+                Some(p) => match p.as_str() {
+                    "-h" | "--help" | "h" | "help" => Ok(Config::Help),
+                    "-v" | "--version" | "v" | "version" => Ok(Config::Version),
+                    _ => Err(format!(
+                        "not found this arguments\n{{ {:?} }}",
+                        options.iter().collect::<Vec<&String>>()
+                    )),
+                },
+                None => Err("Need args".to_string()),
+            },
+        }
+    }
+}
+
+pub struct Inspector {
+    configuration: Config,
+}
+
+impl Inspector {
+    pub fn build(init_options: &Option<Vec<String>>) -> Result<Inspector, String> {
+        let conf = Config::build(init_options);
+        match conf {
+            Ok(conf) => Ok(Inspector {
+                configuration: conf,
+            }),
+            Err(msg) => Err(msg),
+        }
+    }
+    pub fn run(&self) -> Result<(), String> {
+        match &self.configuration {
+            Config::Help => Self::write_help_msg(),
+            Config::Version => Self::write_version_msg(),
+            _ => Err("Error aanlis config".to_string()),
+        }
+    }
+    pub fn write_help_msg() -> Result<(), String> {
+        println!(env!("CARGO_PKG_NAME"));
+        println!(env!("CARGO_PKG_VERSION"));
+        Ok(())
+    }
+    pub fn write_version_msg() -> Result<(), String> {
+        println!(env!("CARGO_PKG_VERSION"));
+        Ok(())
+    }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
